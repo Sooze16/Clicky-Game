52cd01c6a9acad0e4513e16085bea9fa719c0430
Sooze16 <slkpes2002@yahoo.com>
2019-09-07 17:44:10 -0500

    updated logic with Phil

--- client/src/App.js
+++ client/src/App.js
@@ -5,47 +5,104 @@ import Title from "./components/Title";
 import friends from "./friends.json";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
-  state = {
-    friends,
-    score: 0,
-    topScore: 0
-  };
+    // Setting this.state.friends to the friends json array
+    state = {
+        friends,
+        score: 0,
+        topScore: 0
+    }
 
-  clicked = id => {
- 
-      //verify is the card was clicked before
-      // if yes  you need to rest (score,)
-      // if not (update sore, update the top core if new score is better, shuffle)
-      // update the state (setState) this will render 
+    resetClick = () => {
+        const newFriends = this.state.friends.map(friend => {
 
-    this.setState({ friends });
-  };
+            friend.clicked = false;
+            return friend
+        })
+        this.setState({
+            friends: newFriends
+        })
+    }
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
-  render() {
-    return (
-      <Wrapper>
-        <Title
-        score={this.state.score}
-      topScore={this.state.topScore}
-        >
 
-        </Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            myfun={this.clicked}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-      </Wrapper>
-    );
-  }
+    click = id => {
+
+        const updatedFriends = this.state.friends.map(friend => {
+
+            if (id === friend.id) {
+
+
+                if (friend.clicked) {
+                    this.setState({
+                        score: 0
+                    })
+                    this.resetClick()
+
+                } else {
+
+                    if (this.state.score >= this.state.topScore) {
+                        this.setState({
+                            score: this.state.score + 1,
+                            topScore: this.state.topScore + 1
+                        })
+                    } else {
+                        this.setState({
+                            score: this.state.score + 1
+
+                        })
+                    }
+                    if (!friend.clicked) {
+                        friend.clicked = true;
+                    }
+
+
+
+                }
+
+            }
+
+
+            return friend;
+
+        });
+
+        console.log(updatedFriends)
+
+        this.setState({ friends: updatedFriends })
+
+        const NewArray = this.state.friends.sort(() => Math.random() - 0.5)
+
+        this.setState({ friends: NewArray })
+
+
+
+    }
+
+    // Map over this.state.friends and render a FriendCard component for each friend object
+    render() {
+        return ( <Wrapper>
+
+            <Title 
+            Title score = { this.state.score }
+            topScore = { this.state.topScore } >
+
+             
+           </Title>
+            high: { this.state.topScore }
+
+            {
+                this.state.friends.map(friend => ( <
+                    FriendCard myfun = { this.click }
+                    id = { friend.id }
+                    key = { friend.id }
+                    name = { friend.name }
+                    image = { friend.image }
+                    occupation = { friend.occupation }
+                    location = { friend.location }
+                    />
+                ))
+            } </Wrapper>
+        );
+    }
 }
 
 export default App;
