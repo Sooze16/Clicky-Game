33f55e3b79da8062b1db9fe33a1b3aee5b4684fe
Sooze16 <slkpes2002@yahoo.com>
2019-09-07 18:29:23 -0500

    Updates

--- client/src/components/Title/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Title(props) {
-  return <h1 className="title">Score: {props.score} | Top Score:{props.topScore}</h1>;
-}
-
-export default Title;
---
+++ precache-manifest.c6f981cd0c6195860bfffca0506f9064.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1fa4f39f4b3a163fc27f3da4f47088",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "a5e304a460419064c0ec",
+    "url": "/ClickyGame/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "5e83aeb0a3d3423cfe0e",
+    "url": "/ClickyGame/static/js/2.80f08b19.chunk.js"
+  },
+  {
+    "revision": "a5e304a460419064c0ec",
+    "url": "/ClickyGame/static/js/main.f6a7868e.chunk.js"
+  },
+  {
+    "revision": "3f94739a733620af8917",
+    "url": "/ClickyGame/static/js/runtime~main.1df70f2f.js"
+  }
+]);
